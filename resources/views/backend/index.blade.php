117f7793e0f18d21a20b76887cc7f43908792d60
doankhiem <khiembi161219@gmail.com>
2024-04-18 22:42:13 +0700

    clear code back-end

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -1,91 +1,93 @@
 @extends('backend.layouts.master')
-@section('title','E-SHOP || DASHBOARD')
+@section('title','COCOLUX || DASHBOARD')
 @section('main-content')
 <div class="container-fluid">
     @include('backend.layouts.notification')
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-      <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
     </div>
 
     <!-- Content Row -->
     <div class="row">
-      <!-- Products -->
-      <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-success shadow h-100 py-2">
-          <div class="card-body">
-            <div class="row no-gutters align-items-center">
-              <div class="col mr-2">
-                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Products</div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">{{\App\Models\Product::countActiveProduct()}}</div>
-              </div>
-              <div class="col-auto">
-                <i class="fas fa-cubes fa-2x text-gray-300"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- Order -->
-      <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-info shadow h-100 py-2">
-          <div class="card-body">
-            <div class="row no-gutters align-items-center">
-              <div class="col mr-2">
-                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Order</div>
-                <div class="row no-gutters align-items-center">
-                  <div class="col-auto">
-                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{\App\Models\Order::countActiveOrder()}}</div>
-                  </div>
-
+        <!-- Products -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Products</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                {{\App\Models\Product::countActiveProduct()}}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-cubes fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div class="col-auto">
-                <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
+
+        <!-- Order -->
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Order</div>
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                                        {{\App\Models\Order::countActiveOrder()}}</div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
     <div class="row">
 
-      <!-- Area Chart -->
-{{--      <div class="col-xl-8 col-lg-7">--}}
-{{--        <div class="card shadow mb-4">--}}
-{{--          <!-- Card Header - Dropdown -->--}}
-{{--          <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">--}}
-{{--            <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>--}}
+        <!-- Area Chart -->
+        {{--      <div class="col-xl-8 col-lg-7">--}}
+        {{--        <div class="card shadow mb-4">--}}
+        {{--          <!-- Card Header - Dropdown -->--}}
+        {{--          <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">--}}
+        {{--            <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>--}}
 
-{{--          </div>--}}
-{{--          <!-- Card Body -->--}}
-{{--          <div class="card-body">--}}
-{{--            <div class="chart-area">--}}
-{{--              <canvas id="myAreaChart"></canvas>--}}
-{{--            </div>--}}
-{{--          </div>--}}
-{{--        </div>--}}
-{{--      </div>--}}
+        {{--          </div>--}}
+        {{--          <!-- Card Body -->--}}
+        {{--          <div class="card-body">--}}
+        {{--            <div class="chart-area">--}}
+        {{--              <canvas id="myAreaChart"></canvas>--}}
+        {{--            </div>--}}
+        {{--          </div>--}}
+        {{--        </div>--}}
+        {{--      </div>--}}
 
-      <!-- Pie Chart -->
-{{--      <div class="col-xl-4 col-lg-5">--}}
-{{--        <div class="card shadow mb-4">--}}
-{{--          <!-- Card Header - Dropdown -->--}}
-{{--          <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">--}}
-{{--            <h6 class="m-0 font-weight-bold text-primary">Users</h6>--}}
-{{--          </div>--}}
-{{--          <!-- Card Body -->--}}
-{{--          <div class="card-body" style="overflow:hidden">--}}
-{{--            <div id="pie_chart" style="width:350px; height:320px;">--}}
-{{--          </div>--}}
-{{--        </div>--}}
-{{--      </div>--}}
+        <!-- Pie Chart -->
+        {{--      <div class="col-xl-4 col-lg-5">--}}
+        {{--        <div class="card shadow mb-4">--}}
+        {{--          <!-- Card Header - Dropdown -->--}}
+        {{--          <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">--}}
+        {{--            <h6 class="m-0 font-weight-bold text-primary">Users</h6>--}}
+        {{--          </div>--}}
+        {{--          <!-- Card Body -->--}}
+        {{--          <div class="card-body" style="overflow:hidden">--}}
+        {{--            <div id="pie_chart" style="width:350px; height:320px;">--}}
+        {{--          </div>--}}
+        {{--        </div>--}}
+        {{--      </div>--}}
     </div>
     <!-- Content Row -->
 
-  </div>
+</div>
 @endsection
 
 @push('scripts')
@@ -108,7 +110,7 @@
 {{--      chart.draw(data, options);--}}
 {{--  }--}}
 {{--</script>--}}
-  {{-- line chart --}}
+{{-- line chart --}}
 {{--  <script type="text/javascript">--}}
 {{--    const url = "{{route('product.order.income')}}";--}}
 {{--    // Set new default font family and font color to mimic Bootstrap's default styling--}}
